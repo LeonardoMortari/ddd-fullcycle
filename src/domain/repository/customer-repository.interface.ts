4bbc0edb3a0ec6c9393e01a2d460f0d41009ca15
LeoMortarIMDS <leonardo.mortari@imendes.com.br>
2022-03-23 13:59:32 -0300

    Criando projeto

---
+++ src/domain/repository/customer-repository.interface.ts
@@ -0,0 +1,9 @@
+import Customer from "../entity/customer";
+import RepositoryInterface from "./repository-interface";
+
+export default interface CustomerRepositoryInterface 
+    extends RepositoryInterface<Customer> {
+
+    
+
+}
